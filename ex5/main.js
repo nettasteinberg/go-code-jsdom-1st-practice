1b80edf798e07a2636e333a6b2b2cd903689f19e
Netta Steinberg <netta.steinberg@gmail.com>
2023-04-10 22:35:05 +0300

    made the code to act dinamically

--- ex5/main.js
+++ ex5/main.js
@@ -1,24 +1,11 @@
 const body = document.querySelector("body");
 body.style.height = "1000px";
-const black = document.createElement("button");
-black.classList.add("changeBackgroundColor");
-black.textContent = "black";
-body.appendChild(black);
-const green = document.createElement("button");
-green.classList.add("changeBackgroundColor");
-green.textContent = "green";
-body.appendChild(green);
-const red = document.createElement("button");
-red.classList.add("changeBackgroundColor");
-red.textContent = "red";
-body.appendChild(red);
-const blue = document.createElement("button");
-blue.classList.add("changeBackgroundColor");
-blue.textContent = "blue";
-body.appendChild(blue);
-const colors = document.querySelectorAll(".changeBackgroundColor");
-colors.forEach(element => 
-    element.addEventListener("click", function(event) {
-        body.style.backgroundColor = element.textContent;
-    }
-))
+const colors = ["black", "green", "red", "blue"];
+colors.forEach(color => {
+    const button = document.createElement("button");
+    button.textContent = color;
+    button.addEventListener("click", function(event) {
+        body.style.backgroundColor = color;
+    })
+    body.appendChild(button);    
+});
